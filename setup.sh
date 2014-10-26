d30cd39322ca357601c2c936111b62c228996bd0
comealong <root@localhost.localdomain>
2014-10-26 04:38:32 -0400

    changed

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ cp -f _vimrc ~/.vimrc
 mkdir -p ~/.vim/plugin
 
 git clone https://github.com/vim-scripts/echofunc.vim.git
-cp echofunc.vim/plugin/echofunc.vim  ~/.vim/plugin/
+cp -f echofunc.vim/plugin/echofunc.vim  ~/.vim/plugin/
 
 # ctags must be installed
 git clone https://github.com/vim-scripts/OmniCppComplete.git
@@ -15,6 +15,9 @@ cp -f -R OmniCppComplete/* ~/.vim/
 # install python-epydoc, shift+k to read the doc for python
 git clone https://github.com/vim-scripts/Pydiction.git
 cp -f -R Pydiction/* ~/.vim/
+
+git clone https://github.com/vim-scripts/taglist.vim.git
+cp -f -R taglist.vim/plugin/taglist.vim  ~/.vim/plugin/
 
 # libclang-dev muse be installed
 git clone https://github.com/Rip-Rip/clang_complete.git
